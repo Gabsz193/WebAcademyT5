e6948fd694738640ca1642fbce13f8c5c0e4b931
Luiz Gabriel <senaaluizgabriel@gmail.com>
2025-05-08 20:03:50 -0400

    Primeira tarefa de Node, implementando um servidor que mostra os arquivos do diretorio passado por argumento

---
+++ WACAD007-Node/node1/index.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const fs = require("fs");
+
+const PORT = 3000;
+
+if(process.argv.length != 3) {
+    throw new Error("Quantidade de argumentos inválida");
+}
+
+const directory_name = process.argv[2];
+
+let file_names = [];
+
+fs.readdir(directory_name, (err, files) => {
+    if(err) throw new Error("Ocorreu um erro: " + err.message);
+    file_names = files;
+});
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "content-type": "text/html;charset=utf-8" });
+    file_names.forEach((file) => {
+        res.write(file);
+        res.write("</br>");
+    })
+    res.end();
+});
+
+server.listen(PORT);
+
+console.log(`Ouvindo na porta: http://localhost:${PORT}/`);
