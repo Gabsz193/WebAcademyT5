0b97c1422afb1f01e513479c2db62abbfb5f01bd
Luiz Gabriel <senaaluizgabriel@gmail.com>
2025-05-24 09:07:36 -0400

    Tarefa 2 atualizada

--- WACAD009-Express/ExpTS/src/index.ts
+++ WACAD009-Express/ExpTS/src/index.ts
@@ -1,31 +1,16 @@
 import dotenv from 'dotenv';
-import express from 'express';
-import validateEnv from './utils/validateEnv';
-import { logger } from './middlewares/logger';
-import { engine } from 'express-handlebars';
+import express, { Request, Response } from 'express';
 
 dotenv.config();
-
-validateEnv();
 
 const PORT = process.env['PORT'] ?? 5000;
 const app = express();
 
-app.use(logger('simple'));
-
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/views`);
-
-app.get('/', (req, res) => {
-	res.send('Hello');
+app.get('/', (req : Request, res : Response) => {
+	res.send('Hello World');
 });
 
-app.get('/about', (req, res) => {
-	res.send('About us');
-});
-
-app.listen(PORT, (err) => {
+app.listen(PORT, (err : Error | undefined) => {
 	if (err) throw new Error(err.message);
 	console.log(`Listening on http://localhost:${PORT}/`);
 });
