9825373afa54e178ea9a1acd58af6b2cc00ad7d1
Luiz Gabriel <senaaluizgabriel@gmail.com>
2025-05-27 20:23:51 -0400

    Implementação da tarefa 2 parte 10, colocando sass e rotas de static files

--- WACAD009-Express/ExpTS/src/index.ts
+++ WACAD009-Express/ExpTS/src/index.ts
@@ -6,14 +6,15 @@ import loremRouter from "./router/lorem/router";
 import handlebarsRouter from "./router/handlebars/router";
 import mainRouter from "./router/main/router";
 import { engine } from "express-handlebars";
+import helpers from "./views/helpers/helpers";
 import path from "path";
+import sass from "node-sass-middleware";
 
 dotenv.config();
 validateEnv();
 
 const PORT = process.env["PORT"] ?? 5000;
 const app = express();
-import helpers from "./views/helpers/helpers";
 
 app.use(logger("simple"));
 
@@ -33,6 +34,18 @@ app.set("views", path.join(__dirname, "views"));
 app.use(loremRouter);
 app.use(handlebarsRouter);
 app.use(mainRouter);
+
+app.use("/img", express.static(path.join(process.cwd(), "public/img")));
+app.use("/css", express.static(path.join(process.cwd(), "public/css")));
+app.use("/js", express.static(path.join(process.cwd(), "public/js")));
+app.use(
+  sass({
+    src: path.join(process.cwd(), "public/scss"),
+    dest: path.join(process.cwd(), "public/css"),
+    outputStyle: "compressed",
+    prefix: "/css"
+  })
+);
 
 app.listen(PORT, (err: Error | undefined) => {
   if (err) throw new Error(err.message);
